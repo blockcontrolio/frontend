a0a4e8ef45aa4b65f82d586b202e822fda51f9c8
Igor Naumov <naumov@live.com>
2025-05-20 20:01:02 +0300

    Settings: Track whether the apiKey has changed; remove alert

--- src/pages/Settings.vue
+++ src/pages/Settings.vue
@@ -2,14 +2,23 @@
 export default {
   data() {
     return {
-      apiKey: localStorage.getItem('x-api-key') || ''
+      apiKey: localStorage.getItem('x-api-key') || '',
+      originalApiKey: ''
+    }
+  },
+  computed: {
+    hasChanged() {
+      return this.apiKey !== this.originalApiKey;
     }
   },
   methods: {
     save() {
       localStorage.setItem('x-api-key', this.apiKey);
-      alert('API key saved');
+      this.originalApiKey = this.apiKey; // reset on change detection
     }
+  },
+  mounted() {
+    this.originalApiKey = this.apiKey;
   }
 }
 </script>
@@ -19,7 +28,12 @@ export default {
     <h3 class="mb-3">API Settings</h3>
     <input v-model="apiKey" class="form-control mb-2" placeholder="Enter x-api-key"/>
     <div class="d-flex justify-content-end mt-3">
-      <button class="btn btn-success" @click="save">Save</button>
+      <button
+          class="btn btn-success"
+          @click="save"
+          :disabled="!hasChanged">
+        Save
+      </button>
     </div>
   </div>
 </template>
